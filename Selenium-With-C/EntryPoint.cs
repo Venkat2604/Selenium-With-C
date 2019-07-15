3cd4cf9dfa7d34b425f034b7b32601a222e1f7c4
Zac Ranck <MY7H1C@gmail.com>
2019-07-15 01:33:48 -0600

    setup very basic selenium code with mock test site

---
+++ Selenium-With-C/EntryPoint.cs
@@ -0,0 +1,20 @@
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+
+        Thread.Sleep(3000);
+
+        driver.Quit();
+
+    }
+}
+
